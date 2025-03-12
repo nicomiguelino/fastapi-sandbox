ba9e19efbc9c35f9abc2b86f1276cc48bd20cb29
nicomiguelino <nicomiguelino2014@gmail.com>
2025-03-12 14:50:08 -0700

    feat: create a new endpoint for updating an item

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = False
+
 
 @app.get('/')
 async def read_root():
@@ -14,4 +23,16 @@ async def read_item(item_id: int, q: str | None = None):
     return {
         'item_id': item_id,
         'q': q,
+    }
+
+
+@app.put('/items/{item_id}')
+def update_item(
+    item_id: int,
+    item: Item,
+    q: str | None = None,
+):
+    return {
+        'item_id': item_id,
+        'item': item,
     }
